f1b9c73c3e9cead1a088a47a88041e564a9a0ed1
fmartinezfenoll <fmf29@gmail.com>
2024-04-15 16:08:01 +0200

    "Interceptador para inyectar el Access Token"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NavbarComponent,
-    TasksComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, LoginComponent, NavbarComponent, TasksComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,25 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+import { Injectable } from '@angular/core';
+import { HttpRequest,
+ HttpHandler,
+ HttpEvent,
+ HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+ constructor(public userService: UserService) {}
+ intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+ const token = this.userService.getToken();
+ var request = req;
+ //Validamos si el token existe
+ if (token) {
+ //Clonamos el token y lo inyectamos en la cabecera de todas las peticiones HTTP
+ request = req.clone({
+ setHeaders: {
+ authorization: `Bearer ${token}`
+ }
+ });
+ }
+ return next.handle(request);
+ }
+} 
