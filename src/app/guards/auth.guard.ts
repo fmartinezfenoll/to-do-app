c840ed225a0b5964b9fc57e595e3d140d8ce6ed6
fmartinezfenoll <fmf29@gmail.com>
2024-04-15 16:14:04 +0200

    "Autorización: authGuard y canActivate"

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+import { inject } from '@angular/core';
+import {
+  CanActivateFn,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { UserService } from '../services/user.service';
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const router = inject(Router);
+  const userService = inject(UserService);
+  console.log('Guard!');
+  if (userService.isAuth()) {
+    console.log(`Accediendo a home`);
+    return true;
+  } else {
+    console.log('Saltando al login...');
+    router.navigateByUrl('/login');
+    return false;
+  }
 };
