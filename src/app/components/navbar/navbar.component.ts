74626bdb673b4402a8ed9c7806c748e2c2e130e3
fmartinezfenoll <fmf29@gmail.com>
2024-04-22 12:55:38 +0200

    Barra de navegación

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { UserService } from "../../services/user.service";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+ selector: 'app-navbar',
+ templateUrl: './navbar.component.html',
+ styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent implements OnInit {
+ title = 'toDoApp para SD';
+ displayName = '';
+ constructor(public userService: UserService, public router: Router) { }
+ ngOnInit(): void {
+ console.log('ngOnInit: navbar');
+ this.userService.getUser().subscribe({
+ next: user => {this.displayName = user.displayName},
+ error: err => {
+ this.router.navigateByUrl("/login");
+ console.error("Error cargando datos del usuario");
+ console.log(err);
+ }
+ });
+ }
+} 
